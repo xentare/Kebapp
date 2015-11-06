f54c14b83c6730df5e398530ea9ae03fc6052f3f
xentare <H4113@student.jamk.fi>
2015-11-06 14:08:56 +0200

    Enable GPS dialog added

---
+++ app/app/src/main/java/com/example/juha/kebapp/GPSDialogFragment.java
@@ -0,0 +1,36 @@
+package com.example.juha.kebapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+/**
+ * Created by Juha on 6.11.2015.
+ */
+public class GPSDialogFragment extends DialogFragment {
+
+    /**
+     * TODO: Remove hardcoded strings
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Enable GPS?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ((MainActivity)getActivity()).onGPSDialogPositiveClick();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ((MainActivity)getActivity()).onGPSDialogNegativeClick();
+                    }
+                })
+                .create();
+    }
+}
